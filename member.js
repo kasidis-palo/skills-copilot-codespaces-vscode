42187f5e9f69185e59c65313da908d6a4fa1b5ea
Kasidis Chaowvasin <119559399+kasidis-palo@users.noreply.github.com>
2025-01-25 13:14:42 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,5 @@
+function skillsMember() {
+    return {
+        skills: ['JavaScript', 'React', 'Node.js', 'MongoDB', 'Express', 'HTML', 'CSS', 'Bootstrap', 'Materialize', 'jQuery', 'MySQL', 'Sequelize', 'Mongoose', 'Handlebars', 'RESTful APIs', 'GraphQL', 'Apollo Server', 'React Router', 'Redux', 'Context', 'JWT', 'PWA', 'Service Workers', 'IndexedDB', 'Heroku', 'Git', 'GitHub', 'VS Code', 'Jest', 'Mocha', 'Chai', 'WebPack', 'Babel', 'ESLint', 'Prettier', 'Progressive Web Applications', 'Single Page Applications', 'Model-View-Controller', 'Object Oriented Programming', 'Test Driven Development', 'Agile Development', 'Scrum', 'Kanban', 'Pair Programming', 'Project Management', 'Team Collaboration', 'Problem Solving', 'Critical Thinking', 'Adaptability', 'Time Management', 'Attention to Detail']
+    };
+}
